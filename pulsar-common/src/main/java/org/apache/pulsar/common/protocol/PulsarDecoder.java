188a6efc037788eda95dd4f02092a5a1222895b4
rdhabalia <radhabalia@apache.org>
2026-06-23 12:08:59 -0700

    Pulsar: add CommandScan wire command for Streaming Lake scans
    Add SCAN / SCAN_RESPONSE to BaseCommand with CommandScan (topic, date range, opaque
    predicate blob) and CommandScanResponse (records, is_last, error) messages, plus
    PulsarDecoder dispatch and handleScan/handleScanResponse hooks (overridden by the
    broker and client). Verified: pulsar-common compiles.

--- pulsar-common/src/main/java/org/apache/pulsar/common/protocol/PulsarDecoder.java
+++ pulsar-common/src/main/java/org/apache/pulsar/common/protocol/PulsarDecoder.java
@@ -81,6 +81,8 @@ import org.apache.pulsar.common.api.proto.CommandScalableTopicUpdate;
 import org.apache.pulsar.common.api.proto.CommandSeek;
 import org.apache.pulsar.common.api.proto.CommandSend;
 import org.apache.pulsar.common.api.proto.CommandSendError;
+import org.apache.pulsar.common.api.proto.CommandScan;
+import org.apache.pulsar.common.api.proto.CommandScanResponse;
 import org.apache.pulsar.common.api.proto.CommandSendReceipt;
 import org.apache.pulsar.common.api.proto.CommandSubscribe;
 import org.apache.pulsar.common.api.proto.CommandSuccess;
@@ -544,6 +546,16 @@ public abstract class PulsarDecoder extends ChannelInboundHandlerAdapter {
                 handleCommandWatchTcAssignmentsClose(cmd.getWatchTcAssignmentsClose());
                 break;
 
+            case SCAN:
+                checkArgument(cmd.hasScan());
+                handleScan(cmd.getScan());
+                break;
+
+            case SCAN_RESPONSE:
+                checkArgument(cmd.hasScanResponse());
+                handleScanResponse(cmd.getScanResponse());
+                break;
+
             default:
                 break;
             }
@@ -629,6 +641,14 @@ public abstract class PulsarDecoder extends ChannelInboundHandlerAdapter {
     }
 
     protected void handleAckResponse(CommandAckResponse ackResponse) {
+        throw new UnsupportedOperationException();
+    }
+
+    protected void handleScan(CommandScan scan) {
+        throw new UnsupportedOperationException();
+    }
+
+    protected void handleScanResponse(CommandScanResponse scanResponse) {
         throw new UnsupportedOperationException();
     }
 
